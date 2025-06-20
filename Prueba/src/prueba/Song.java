fe198f63f8cb6397cc308b46cd930a982496f7de
JustPreo <impre0hn@gmail.com>
2025-06-20 13:50:15 -0600

    A

---
+++ Prueba/src/prueba/Song.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package prueba;
+
+import javax.swing.ImageIcon;
+
+/**
+ *
+ * @author user
+ */
+public class Song {
+    
+    private int codigo;
+    private String nombre;
+    private double precio;
+    private int sumaEstrellas;
+    private int totalReviews;
+    private ImageIcon imagenDisco;
+
+    public Song(int codigo, String nombre, double precio,ImageIcon imagenDisco) {
+        this.codigo = codigo;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.imagenDisco = imagenDisco;
+        this.sumaEstrellas = 0;
+        this.sumaEstrellas = 0;
+        
+
+    }
+
+    
+
+}
